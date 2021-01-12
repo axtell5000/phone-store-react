1946c6c9b09cc66c4be1cd2712de7bb90ec20f25
Stephen Axtell <axtell.stephen@gmail.com>
2021-01-12 15:03:42 +0200

    created navbar using styled component and a generic button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components/macro';
+
+import { ButtonContainer } from './Button';
+import logo from "../logo.svg";
 
 export default class Navbar extends Component {
   render() {
     return (
       <div>
-        <h3>Hello from the Navbar</h3>
+        <Nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+          {/* 
+          https://www.iconfinder.com/icons/1243689/call_phone_icon
+          Creative Commons (Attribution 3.0 Unported);
+          https://www.iconfinder.com/Makoto_msk 
+          */}
+          <Link to="/" title="HOme">
+            <img src={logo} alt="store" className="navbar-brand" />
+          </Link>
+          
+          <ul className="navbar-nav align-items-center">
+            <li className="nav-item ml-5">
+              <Link to="/" className="nav-link" title="Products">
+                products
+              </Link>
+            </li>
+          </ul>
+          <Link to="/cart" className="ml-auto" title="To Cart">
+            <ButtonContainer>
+              <span className="mr-2">
+                <i className="fas fa-cart-plus"/>
+              </span>
+              my cart
+            </ButtonContainer>
+          </Link>
+        </Nav>
       </div>
     )
   }
 }
+
+const Nav = styled.nav`
+  background: var(--mainBlue);
+
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size:1.3rem;
+    text-transform:capitalize;
+  }
+
+  @media (max-width: 576px) {
+    .navbar-nav {
+      flex-direction: row !important;
+    }
+  }
+`;
