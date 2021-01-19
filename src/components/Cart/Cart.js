b64704de97f620b48689a928eece6928f031d4fa
Stephen Axtell <axtell.stephen@gmail.com>
2021-01-19 14:14:23 +0200

    adding logic for checking if cart is empty

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import { ProductConsumer } from '../../context';
 
 import Title from '../Title';
 import CartCols from './CartCols';
+import EmptyCart from './EmptyCart';
 
 export default class Cart extends Component {
   render() {
     return (
       <section>
-        <Title name="your" title="cart" />
-        <CartCols />
+        <ProductConsumer>
+          {
+            value => {
+              const {cart} = value;
+              if (cart.length > 0) {
+                return (
+                  <Fragment>
+                    <Title name="your" title="cart" />
+                    <CartCols />
+                  </Fragment>
+                );
+              } else {
+                return ( <EmptyCart />)
+              }
+            }
+          }
+        </ProductConsumer>
       </section>
     );
   }
